765017a07fcf9c6d1ced4effdc9a269dd40a4a0c
starklow <94318471+starklow@users.noreply.github.com>
2021-11-15 12:47:13 -0300

    Update Config.lua

--- ext/Shared/Config.lua
+++ ext/Shared/Config.lua
@@ -25,13 +25,13 @@ local s_IGUID_ApcsSplash = Guid("37947774-F524-41E7-9207-DA3CDD0D2FFE")
 ResourceManager:RegisterInstanceLoadHandler(s_PGUID_ApcsSplash, s_IGUID_ApcsSplash, function(p_LoadedInstance)
     local s_VeniceExplosionEntityData = VeniceExplosionEntityData(p_LoadedInstance)
     s_VeniceExplosionEntityData:MakeWritable()
-    s_VeniceExplosionEntityData.innerBlastRadius = 20.0
-    s_VeniceExplosionEntityData.blastDamage = 99.9
-    s_VeniceExplosionEntityData.blastRadius = 20.0
-    s_VeniceExplosionEntityData.shockwaveDamage = 50.00
-    s_VeniceExplosionEntityData.shockwaveRadius = 20.0
-    s_VeniceExplosionEntityData.shockwaveImpulse = 100.0
-    s_VeniceExplosionEntityData.shockwaveTime = 1.00
+    s_VeniceExplosionEntityData.innerBlastRadius = 7.0
+    s_VeniceExplosionEntityData.blastDamage = 100.0
+    s_VeniceExplosionEntityData.blastRadius = 10.0
+    s_VeniceExplosionEntityData.shockwaveDamage = 100.0
+    s_VeniceExplosionEntityData.shockwaveRadius = 4.0
+    s_VeniceExplosionEntityData.shockwaveImpulse = 40.0
+    s_VeniceExplosionEntityData.shockwaveTime = 0.20
 print("changed VeniceExplosionEntityData of Apc's Splash")
 end)
 
